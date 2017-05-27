f4adaed66461a9071118f4870aff1b53f85cf595
Youssef Habri <youssef.habri@gmail.com>
2017-05-27 15:02:55 +0000

    remove target folder

--- target/debug/chip8.d
+++
@@ -1,1 +0,0 @@
-/home/habri/Development/Rust/chip-8.rs/target/debug/chip8: /home/habri/Development/Rust/chip-8.rs/src/main.rs /home/habri/Development/Rust/chip-8.rs/src/display.rs /home/habri/Development/Rust/chip-8.rs/src/cpu.rs /home/habri/Development/Rust/chip-8.rs/src/keypad.rs
